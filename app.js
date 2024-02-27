2d6bb127bc713405c12586718111b6211edf2c46
[Hassan uddin] <[uddinhassan676@gmail.com]>
2024-02-27 00:39:40 -0800

    Assignent 15 java script practice

---
+++ app.js
@@ -0,0 +1,156 @@
+//chapter 6
+
+//Q1
+
+x += 1;
+
+
+//Q2
+
+x -= 1;
+
+//Q3
+
+var x = 50;
+var y = x++;
+
+y = 50
+
+//Q4
+
+var y = 50;
+var z = --y;
+
+z = 49
+
+//Q5
+
+var newNum = --num;
+
+//Q6
+
+var originalValue = x++;
+
+//Q7
+
+var num = 10;
+
+num++;
+
+alert("The new value is: " + num);
+
+////////////////////////////////
+
+//chapter 7
+
+
+//Q1
+
+calculatedNum is 14
+
+//Q2
+
+calculatedNum is 24
+
+//Q3
+
+calculatedNum will have a value of 4 * 6 = 24
+
+//Q4
+
+calculatedNum will have a value of (4 * 4) + 2 = 16 + 2 = 18
+
+
+//Q5
+
+var cost = (2 + 2) * 4 + 10;
+
+//Q6
+
+var units = 2 + (2 * 4) + 10;
+
+//Q7
+
+var pressure = (4 / 2) * 4;
+
+
+/////////////////////////////////////
+
+//chapter 8
+
+//Q1
+
+num is "22"
+
+//Q2
+
+var message = ("Hello," + "Dolly");
+alert(message);
+"Hello,Dolly"
+
+//Q3
+
+alert("33" + 3);
+
+333
+
+//Q4
+
+alert("Pakistan" + " Zindabad");
+
+//Q5
+
+var concatenated = "The number is: " + 5;
+
+//Q6
+
+
+var firstString = "Hello";
+var secondString = "World";
+
+
+var concatenatedString = firstString + secondString;
+
+
+console.log(concatenatedString);
+
+////////////////////////////////////////
+
+//chapter 9
+
+
+//Q1
+
+var firstName = prompt("Enter first name");
+
+//Q2
+
+var country = prompt("Country?", "China");
+
+//Q3
+
+var yourName = prompt("Enter Your Name");
+
+//Q4
+
+var userInput = prompt("Please enter your favorite color", "Blue");
+
+//Q5
+
+
+var message = "Enter your name";
+var defaultResponse = "John Doe";
+
+
+var userInput = prompt(message, defaultResponse);
+
+//Q6
+
+// 
+var userInput = prompt("Please enter your name", "John Doe");
+
+alert("You entered: " + userInput);
+
+///////////end////////////
+
+
